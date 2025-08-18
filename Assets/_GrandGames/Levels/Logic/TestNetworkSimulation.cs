9c7d339cc9a53b524b3fd49031dbd1bc9fe41c86
Muhammed Erdogan <muhammedea33@gmail.com>
2025-08-18 23:48:32 +0300

    feat: add TestNetworkSimulation class for network request testing

---
+++ Assets/_GrandGames/Levels/Logic/TestNetworkSimulation.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class TestNetworkSimulation : MonoBehaviour
+{
+    //local data path: Assets/_GrandGames/Resources/resources_levels_1_500
+    //remote data path: Assets/_GrandGames/Levels/levels_1_500
+
+    private static readonly Regex LevelNameRegex = new("^[a-zA-Z0-9_]+$", RegexOptions.Compiled | RegexOptions.CultureInvariant);
+
+    private void Start()
+    {
+    }
+
+    [ContextMenu("Test")]
+    private async void Test()
+    {
+        var url = "https://raw.githubusercontent.com/typicode/demo/master/db.json";
+        var ct = this.GetCancellationTokenOnDestroy();
+
+        var lastModifiedDate = await GetLastModifiedDate(url, ct);
+        Debug.Log($"Last-Modified date for {url} is {lastModifiedDate}");
+    }
+
+    private static async UniTask<DateTime> GetLastModifiedDate(string url, CancellationToken ct)
+    {
+        var currentLevelIndex = 1;
+
+        var (status, headers) = await HeadAsync(url, 10, ct);
+
+        headers.TryGetValue("Content-Type", out var contentType);
+        headers.TryGetValue("Content-Length", out var contentLength);
+
+        headers.TryGetValue("Last-Modified", out var lastModified);
+        headers.TryGetValue("ETag", out var etag);
+
+        Debug.Log($"HEAD request to {url} returned status {status}");
+
+        return DateTime.TryParse(lastModified, out var lastModifiedDate) ?
+            lastModifiedDate :
+            DateTime.MinValue;
+    }
+
+    public static async UniTask<(long status, Dictionary<string, string> headers)> HeadAsync(
+        string url,
+        int timeoutSec = 10,
+        CancellationToken ct = default,
+        IDictionary<string, string> requestHeaders = null)
+    {
+        using var req = UnityWebRequest.Head(url);
+        req.timeout = timeoutSec;
+
+        if (requestHeaders != null)
+        {
+            foreach (var kv in requestHeaders)
+            {
+                req.SetRequestHeader(kv.Key, kv.Value);
+            }
+        }
+
+        await req.SendWebRequest().ToUniTask(cancellationToken: ct);
+
+#if UNITY_2020_1_OR_NEWER
+        var ok = req.result == UnityWebRequest.Result.Success;
+#else
+        bool ok = !(req.isNetworkError || req.isHttpError);
+#endif
+        if (!ok)
+        {
+            throw new Exception($"HEAD failed ({req.responseCode}): {req.error}");
+        }
+
+        return (req.responseCode, req.GetResponseHeaders());
+    }
+}
