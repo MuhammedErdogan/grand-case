0034c5eabe9d0a88e6e94b09519be4c07fa4caae
Muhammed Erdogan <muhammedea33@gmail.com>
2025-08-18 23:15:23 +0300

    refactor: rename domain files and resources path

---
+++ Assets/_GrandGames/Levels/Logic/Util/JsonUtil.cs
@@ -0,0 +1,16 @@
+using System.Text;
+using UnityEngine;
+
+namespace _GrandGames.Levels.Util
+{
+    public static class JsonUtil
+    {
+        public static T FromUtf8<T>(byte[] bytes, int len)
+        {
+            var json = Encoding.UTF8.GetString(bytes, 0, len);
+            return JsonUtility.FromJson<T>(json);
+        }
+
+        public static T FromString<T>(string json) => JsonUtility.FromJson<T>(json);
+    }
+}
